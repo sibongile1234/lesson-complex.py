576891adfc9f2f954c203e38478145d477646987
sibongile1234 <sibongilemsoni@icloud.com>
2025-11-03 12:20:46 -0800

    changed less to no girls

---
+++ lesson complex.py
@@ -0,0 +1,10 @@
+username=input("enter your username")
+if len(username)<3:
+    print(f" this {username} is too short")
+else:
+    print("welcome username")
+girls=["sibo","grace","lala","fadzai","tasha"] 
+if len(girls)<5:
+    print(f"there alot of {girls}")  
+else:
+    print(F"there no {girls}")  
